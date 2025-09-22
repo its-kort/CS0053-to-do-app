6d2a77bba89c4de84badeb286974770970c16ad7
17eb <deavinsanjuan@gmail.com>
2025-09-22 15:29:20 +0800

    Fixed indentation of functions, faulty remove_task fixed

--- toDoApp.py
+++ toDoApp.py
@@ -2,36 +2,36 @@
 
 tasks=[]
 
-def addtask(task) :
-  tasks.append(task)
-  print("task added!")
+def add_task(task) :
+    tasks.append(task)
+    print("task added!")
 
-def showTasks( ):
+def show_Tasks( ):
     if len(tasks)==0 :
-      print("no tasks yet")
+        print("no tasks yet")
     else:
-     for i in range (len(tasks)):
-      print(i+1,".",tasks[i])
+        for i in range (len(tasks)):
+            print(i+1,".",tasks[i])
 
-def removetask(tasknumber):
+def remove_task(tasknumber):
     tasks.pop(tasknumber) 
     print("task removed!!")
 
 def main():
     while True:
-        print("1 Add Task")
-        print("2.Show Tasks")
-        print("3.Remove Task")
-        print("4- Exit")
+        print("1. Add Task")
+        print("2. Show Tasks")
+        print("3. Remove Task")
+        print("4. Exit")
         ch = input("enter choice : ")
         if ch=="1":
             t = input("enter task : ")
-            addtask(t)
+            add_task(t)
         elif ch=="2":
-            showTasks()
+            show_Tasks()
         elif ch=="3":
-            n=int(input("enter task no to remove: "))
-            removetask(n)   
+            n=int(input("enter task no. to remove: "))
+            remove_task(n-1)   
         elif ch=="4":
             break;
         else:
