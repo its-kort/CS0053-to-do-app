64eae0800e1a4e5860fce44a3e331618badb2ade
kurt <131219459+its-kort@users.noreply.github.com>
2025-09-22 14:35:57 +0800

    Added input validation on main() function, fixed spacing, and renamed functions and variables
    Added:
    - Input validation on user input on removing a task from the menu.
    - Additional line breaks, indentations, and spaces for readability.
    
    Renamed:
    - addtask -> add_task
    - showTasks -> show_tasks
    - removetask -> remove_task
    - ch -> choice
    - t -> task
    - n -> task_number
    
    Fixed:
    - main() function declaration
    
    Removed:
    - Semicolon on break keyword.

--- toDoApp.py
+++ toDoApp.py
@@ -1,21 +1,25 @@
 # toDoApp.py
 
-tasks=[]
+tasks = []
 
-def addtask(task) :
-  tasks.append(task)
-  print("task added!")
 
-def showTasks( ):
-    if len(tasks)==0 :
-      print("no tasks yet")
+def add_task(task):
+    tasks.append(task)
+    print("task added!")
+
+
+def show_tasks():
+    if len(tasks) == 0:
+        print("no tasks yet")
     else:
-     for i in range (len(tasks)):
-      print(i+1,".",tasks[i])
+        for i in range(len(tasks)):
+            print(i+1, ".", tasks[i])
 
-def removetask(tasknumber):
-    tasks.pop(tasknumber) 
+
+def remove_task(task_number):
+    tasks.pop(task_number)
     print("task removed!!")
+
 
 def main():
     while True:
@@ -23,17 +27,24 @@ def main():
         print("2.Show Tasks")
         print("3.Remove Task")
         print("4- Exit")
-        ch = input("enter choice : ")
-        if ch=="1":
-            t = input("enter task : ")
-            addtask(t)
-        elif ch=="2":
-            showTasks()
-        elif ch=="3":
-            n=int(input("enter task no to remove: "))
-            removetask(n)   
-        elif ch=="4":
-            break;
+        choice = input("enter choice : ")
+        
+        if choice == "1":
+            task = input("enter task : ")
+            add_task(task)
+        elif choice == "2":
+            show_tasks()
+        elif choice == "3":
+            try:
+                task_number = int(input("Enter task number to remove: "))
+                remove_task(task_number)
+            except ValueError:
+                print("Please enter a valid number.")
+        elif choice == "4":
+            break
         else:
             print("wrong choice!!")
-main()
+
+
+if __name__ == "__main__":
+    main()
